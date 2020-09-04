2bb717c0e40243b28793fa585d3d859cc28b8468
Paula <pauladenouden@gmail.com>
2020-09-04 23:23:38 +0200

    add routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,26 @@
 import { Router } from 'express';
+import { WordFrequencyAnalyzer } from '../WordFrequencyAnalyzer';
 
-const router = Router();
+const wordFrequencyRouter = Router();
+const wordFrequencyAnalyzer = new WordFrequencyAnalyzer();
 
-router.get('/', (request, response) => {
+wordFrequencyRouter.get('/', (request, response) => {
   return response.json("OK");
 });
 
-export default router;
+wordFrequencyRouter.post('/getHighestFrequency', (request, response) => {
+  const frequency = wordFrequencyAnalyzer.calculateHighestFrequency(request.body.text)
+  return response.json(frequency);
+});
+
+wordFrequencyRouter.post('/getMostFrequentNWords', (request, response) => {
+  const mostFrequentWords = wordFrequencyAnalyzer.calculateMostFrequentNWords(request.body.text, request.body.n)
+  return response.json(mostFrequentWords);
+});
+
+wordFrequencyRouter.post('/getFrequencyForWord', (request, response) => {
+  const frequency = wordFrequencyAnalyzer.calculateFrequencyForWord(request.body.text, request.body.word)
+  return response.json(frequency);
+});
+
+export default wordFrequencyRouter;
